0e12062f2ef6317d236d35ea5bafddff0c266219
Briankaiserx <Briankaiser@mail.ru>
2022-03-15 22:20:02 +0400

    Hexlet

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/f5bfd9539136468cdcf0/maintainability)](https://codeclimate.com/github/Briankaiserx/java-project-lvl4/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/f5bfd9539136468cdcf0/test_coverage)](https://codeclimate.com/github/Briankaiserx/java-project-lvl4/test_coverage)
 [![Actions Status Main](https://github.com/Briankaiserx/java-project-lvl4/actions/workflows/main.yml/badge.svg)](https://github.com/Briankaiserx/java-project-lvl4/actions/workflows/main.yml/badge.svg)
+####
+https://dashboard.heroku.com/apps/hexlet-exercice-java4
---
+++ src/main/java/hexlet/code/App.java
@@ -0,0 +1,71 @@
+package hexlet.code;
+
+import hexlet.code.controllers.RootController;
+import hexlet.code.controllers.UrlController;
+import io.javalin.Javalin;
+import io.javalin.plugin.rendering.template.JavalinThymeleaf;
+
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+
+import static io.javalin.apibuilder.ApiBuilder.path;
+import static io.javalin.apibuilder.ApiBuilder.get;
+import static io.javalin.apibuilder.ApiBuilder.post;
+
+
+public class App {
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "5000");
+        return Integer.parseInt(port);
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(getPort());
+    }
+
+    private static TemplateEngine getTemplateEngine() {
+        TemplateEngine templateEngine = new TemplateEngine();
+
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/templates/");
+
+        templateEngine.addTemplateResolver(templateResolver);
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(new Java8TimeDialect());
+
+        return templateEngine;
+    }
+
+    private static void addRoutes(Javalin app) {
+        app.get("/", RootController.welcome);
+        app.routes(() -> { path("urls", () -> {
+                get(UrlController.listURLs);
+                post(UrlController.createUrl);
+                path("{id}", () -> { get(UrlController.showUrl);
+                    post("/checks", UrlController.checkUrl);
+                });
+            });
+        });
+    }
+
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(config -> {
+            config.enableDevLogging();
+            config.enableWebjars();
+            JavalinThymeleaf.configure(getTemplateEngine());
+        });
+
+        addRoutes(app);
+
+        app.before(ctx -> {
+            ctx.attribute("ctx", ctx);
+        });
+
+        return app;
+    }
+
+}
